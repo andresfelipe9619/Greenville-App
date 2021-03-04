8b0b2bf7fc4480cc3fae6440bda330d15f3b1b57
Andres <andresfelipe9619@gmail.com>
2021-03-04 05:49:57 -0500

    :lipstick: Add accordion to files

--- src/client/components/form-page/FilesFields.js
+++ src/client/components/form-page/FilesFields.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import Divider from '@material-ui/core/Divider';
+import Box from '@material-ui/core/Box';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Dropzone from '../dropzone/Dropzone';
 
 export default function FilesFields({
@@ -12,30 +16,39 @@ export default function FilesFields({
 }) {
   if (!filesGroups || !filesGroups.length) return null;
   return (
-    <Grid
-      item
-      xs={12}
-      container
-      spacing={8}
-      justify="center"
-      alignItems="center"
-    >
-      <Grid item xs={12}>
-        <Typography variant="h5">Files to Upload</Typography>
-      </Grid>
-      <Divider variant="middle" />
-      {filesGroups.map(f => (
-        <Grid item xs={4} key={f.name}>
-          <Typography variant="h6">{f.name}</Typography>
-          <Dropzone
-            field={f.name}
-            values={values}
-            disabled={isLoading}
-            setFieldValue={setFieldValue}
-            // accept={SUPPORTED_FORMATS}
-          />
-        </Grid>
-      ))}
-    </Grid>
+    <Box width="100%" mt={8}>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="files-content"
+          id="files-header"
+        >
+          <Typography variant="h5">Files to Upload</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Grid
+            item
+            xs={12}
+            container
+            spacing={2}
+            justify="center"
+            alignItems="center"
+          >
+            {filesGroups.map(f => (
+              <Grid item xs={4} key={f.name}>
+                <Typography variant="h6">{f.name}</Typography>
+                <Dropzone
+                  field={f.name}
+                  values={values}
+                  disabled={isLoading}
+                  setFieldValue={setFieldValue}
+                  // accept={SUPPORTED_FORMATS}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
   );
 }
